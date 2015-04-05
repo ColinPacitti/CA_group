d2ae821e2ba8ad0d6233719ac3c38394f87e0f25
newpro <w89li@student.cs.uwaterloo.ca>
2015-04-05 17:06:42 -0400

    nameserver debug

--- nameserver.cc
+++ nameserver.cc
@@ -1,20 +1,30 @@
 #include "nameserver.h"
+
 #include <assert.h>
 #include "vendingmachine.h"
 #include "printer.h"
 using namespace std;
 
 NameServer::NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents )
-  :prt(prt),numVendingMachines(numVendingMachines),numStudents(numStudents),matchNext(numVendingMachines-1)
-{}
+  :prt(prt),numVendingMachines(numVendingMachines),numStudents(numStudents),matchNext(numVendingMachines-1),fillposition(0)
+{
+  machines=new VendingMachine*[numVendingMachines];
+}
+
+NameServer::~NameServer(){
+  delete []machines;
+}
 
 void NameServer::VMregister(VendingMachine* vendingmachine){
-  machines.push_back(vendingmachine);
+  //machines.push_back(vendingmachine);
+  machines[fillposition]=vendingmachine;
+  fillposition++;
+  assert(fillposition<=numVendingMachines+1);//exceed max amount
 }
 
 void NameServer::advance(){
-  assert(numVendingMachine!=0);
-  if(matchNext==(numVendingMachine-1)){
+  assert(numVendingMachines!=0);
+  if(matchNext==(numVendingMachines-1)){
     matchNext=0;
   }
   else{
@@ -24,6 +34,7 @@ void NameServer::advance(){
 
 VendingMachine* NameServer::getMachine( unsigned int id ){
   advance();
+  assert(matchNext<fillposition);//within position i filled
   return machines[matchNext];
 }
 
@@ -33,10 +44,13 @@ VendingMachine** NameServer::getMachineList(){
 
 void NameServer::main(){
   while(true){
-    mysize=machines.size();
+    //mysize=machines.size();
     
     _Accept(~NameServer) break;
-    or _When(mysize==numVendingMachine) _Accept(getMachine,getMachineList);
-    or _When(true) _Accept(VMregister);//??? right way?
+    //or _When(==numVendingMachine) _Accept(getMachine,getMachineList);
+    //or _When(true) _Accept(VMregister);//??? right way?
+    or _When(matchNext<=fillposition) _Accept(getMachine);
+    or _When(fillposition==(numVendingMachines)) _Accept(getMachineList);
+    or _Accept(VMregister);
   }
 }
--- nameserver.h
+++ nameserver.h
@@ -2,7 +2,7 @@
 #define __NAMESERVER_H__
 
 //#include "printer.h"
-#include <vector>
+//#include <vector>
 
 _Task VendingMachine;
 _Monitor Printer;
@@ -15,11 +15,14 @@ _Task NameServer {
     unsigned int numStudents;
 
     unsigned int matchNext;//store the next number of Vending machine that should get match
-    std::vector<VendingMachine*>machines;
+    unsigned int fillposition;//store the position to fill next
+    //std::vector<VendingMachine*>machines;
+    VendingMachine** machines;
     void advance();//advance one position for matchNext
     
   public:
     NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents );
+    ~NameServer();
     void VMregister( VendingMachine *vendingmachine );
     VendingMachine *getMachine( unsigned int id );
     VendingMachine **getMachineList();
